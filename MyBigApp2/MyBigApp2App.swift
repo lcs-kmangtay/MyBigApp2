a6c9fccae1244db2c04c3f6a142285459f39e456
Kaya Mangtay <kmangtay@lcs.on.ca>
2026-03-02 11:20:23 -0500

    Initial Commit

---
+++ MyBigApp2/MyBigApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigApp2App.swift
+//  MyBigApp2
+//
+//  Created by Seyhan Kaya Mangtay on 2026-03-02.
+//
+
+import SwiftUI
+
+@main
+struct MyBigApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
