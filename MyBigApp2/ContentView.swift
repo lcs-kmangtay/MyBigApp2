3c4ab0acdb81ca989f413bd4420e2617971a884e
Kaya Mangtay <kmangtay@lcs.on.ca>
2026-03-02 11:45:29 -0500

    First commit in a while, should have earlier, Have completed alot of the Structure of one "post" Including Photo (just a rectangle rn) Subtitle, post/writing, time posted, "more" button.... Will add Custom subview now

--- MyBigApp2/ContentView.swift
+++ MyBigApp2/ContentView.swift
@@ -10,12 +10,53 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                VStack {
+                    Rectangle()
+                        .frame ( width : 200 , height : 200 )
+                    Text("The Globe and Mail")
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .background(.black)
+                        
+                    Text("Maritime farmers holding breath as record dry spring wrings region")
+                    HStack {
+                       Text("1h ago")
+                        Spacer()
+                       Text("...")
+                        
+                    }
+                }
+              
+            
+                VStack{
+                    Rectangle()
+                        .frame ( width : 200 , height : 200 )
+                    Text("The Globe and Mail")
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .background(.black)
+                        
+                    Text("Maritime farmers holding breath as record dry spring wrings region")
+                    HStack {
+                       Text("1h ago")
+                        Spacer()
+                       Text("...")
+                        
+                    }
+                    
+                }
+            }
         }
+        Spacer()
         .padding()
+    }
+}
+
+
+struct ExtractedView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
     }
 }
 
