1189e008277a0bca94c9657c3208442c33102459
Kaya Mangtay <kmangtay@lcs.on.ca>
2026-03-02 12:23:10 -0500

    added newsPostTitle, newsWriting, timePosted and TRIED to add a colour changing option but failed

--- MyBigApp2/ContentView.swift
+++ MyBigApp2/ContentView.swift
@@ -11,48 +11,14 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
-            // first set of two news posts
-            HStack {
-                VStack {
-                    UnevenRoundedRectangle(cornerRadii: .init(topLeading: 35 , topTrailing: 35))
-                        .frame ( width : 200 , height : 200 )
-                    Text("The Globe and Mail")
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(.white)
-                        .background(.black)
-                        
-                    Text("Maritime farmers holding breath as record dry spring wrings region")
-                    HStack {
-                       Text("1h ago")
-                        Spacer()
-                        Image(systemName: "ellipsis")
-                        
-                    }
-                }
-              
             
-                VStack{
-                    UnevenRoundedRectangle(cornerRadii: .init(topLeading: 35 , topTrailing: 35))
-                        .fill(.green)
-                        .frame ( width : 200 , height : 200 )
-                    Text("REUTERS")
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(.white)
-                    
-                        .background(.black)
-                        
-                    Text("Maritime farmers holding breath as record dry spring wrings region")
-                    HStack {
-                       Text("1h ago")
-                        Spacer()
-                        Image(systemName: "ellipsis")
-                        
-                    }
-                    
-                }
-            }
-            // second set of two news posts ( a custom subview)
-            ExtractedView()
+            // first set of two news posts ( a custom subview)
+            ExtractedView(
+                selectedColour: "yellow",
+                newsPostTitle: "The Globe and Mail",
+                newsWriting: "Maritime farmers holding breath as record dry spring wrings region",
+                timePosted: "1h ago"
+            )
             
         }
         Spacer()
@@ -62,19 +28,33 @@ struct ContentView: View {
 
 
 struct ExtractedView: View {
+    
+    
+    //let newsPhoto: Shape
+    //ignoring the photo section right now, I have it as a shape, (rectangle)
+    //lets still add a colour cahnging option for fun, to mimick the photo selection
+    let selectedColour : String
+    let newsPostTitle: String
+    let newsWriting: String
+    let timePosted: String
+    //dont have to put anything for ellipsis because it is not a changing variable for each news post.
+    
+    
+    
     var body: some View {
         HStack {
             VStack {
                 UnevenRoundedRectangle(cornerRadii: .init(topLeading: 35 , topTrailing: 35))
+                    .fill(.(selectedColour))
                     .frame ( width : 200 , height : 200 )
-                Text("The Globe and Mail")
+                Text((newsPostTitle))
                     .frame(maxWidth: .infinity)
                     .foregroundColor(.white)
                     .background(.black)
                     
-                Text("Maritime farmers holding breath as record dry spring wrings region")
+                Text((newsWriting))
                 HStack {
-                   Text("1h ago")
+                   Text((timePosted))
                     Spacer()
                     Image(systemName: "ellipsis")
                     
